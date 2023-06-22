549f8659770928f1676417bdbe53f30dcd1152c5
DeepakIbitwar <127574226+DeepakIbitwar@users.noreply.github.com>
2023-06-22 13:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+//your code here
+const textbox = document.getElementById('evaluatedText');
+const letterCountElement = document.getElementById('letterCount');
+ 
+textbox.addEventListener('input', () => {
+    const letterCount = textbox.value.length;
+ 
+    letterCountElement.textContent = letterCount;
+});
